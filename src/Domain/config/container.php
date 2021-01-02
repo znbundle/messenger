b3a5e155157bf22ae801c1ed6c874694658dcb0c
Vitaliy <theyamshikov@yandex.ru>
2021-01-02 17:53:04 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'ZnBundle\Messenger\Domain\Interfaces\ChatServiceInterface' => 'ZnBundle\Messenger\Domain\Services\ChatService',
     'ZnBundle\Messenger\Domain\Interfaces\Services\MessageServiceInterface' => 'ZnBundle\Messenger\Domain\Services\MessageService',
+    'ZnBundle\Messenger\Domain\Interfaces\ChatServiceInterface' => 'ZnBundle\Messenger\Domain\Services\ChatService',
     'ZnBundle\Messenger\Domain\Interfaces\ChatRepositoryInterface' => 'ZnBundle\Messenger\Domain\Repositories\Eloquent\ChatRepository',
     'ZnBundle\Messenger\Domain\Interfaces\Repositories\MessageRepositoryInterface' => 'ZnBundle\Messenger\Domain\Repositories\Eloquent\MessageRepository',
     'ZnBundle\Messenger\Domain\Interfaces\MemberRepositoryInterface' => 'ZnBundle\Messenger\Domain\Repositories\Eloquent\MemberRepository',
