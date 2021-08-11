8b3677b77610044c1be577a44e5d2ba1ea92ea04
Vitaliy <theyamshikov@yandex.ru>
2021-08-11 11:44:58 +0600

    upd

---
+++ src/Symfony4/Web/views/message/_contacts.php
@@ -0,0 +1,74 @@
+<ul class="contacts-list">
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user1-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    Count Dracula
+                    <small class="contacts-list-date float-right">2/28/2015</small>
+                  </span>
+                <span class="contacts-list-msg">How have you been? I was...</span>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user7-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    Sarah Doe
+                    <small class="contacts-list-date float-right">2/23/2015</small>
+                  </span>
+                <span class="contacts-list-msg">I will be waiting for...</span>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user3-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    Nadia Jolie
+                    <small class="contacts-list-date float-right">2/20/2015</small>
+                  </span>
+                <span class="contacts-list-msg">I'll call you back at...</span>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user5-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    Nora S. Vans
+                    <small class="contacts-list-date float-right">2/10/2015</small>
+                  </span>
+                <span class="contacts-list-msg">Where is your new...</span>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user6-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    John K.
+                    <small class="contacts-list-date float-right">1/27/2015</small>
+                  </span>
+                <span class="contacts-list-msg">Can I take a look at...</span>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user8-128x128.jpg">
+            <div class="contacts-list-info">
+                  <span class="contacts-list-name">
+                    Kenneth M.
+                    <small class="contacts-list-date float-right">1/4/2015</small>
+                  </span>
+                <span class="contacts-list-msg">Never mind I found...</span>
+            </div>
+        </a>
+    </li>
+</ul>
--- src/Symfony4/Web/views/message/index.php
+++ src/Symfony4/Web/views/message/index.php
@@ -18,10 +18,7 @@
  * @var $this \ZnLib\Web\View\View
  */
 
-use ZnCore\Base\Legacy\Yii\Helpers\Url;
-use ZnCore\Base\Libs\I18Next\Facades\I18Next;
 use ZnCore\Domain\Libs\DataProvider;
-use ZnLib\Web\Widgets\Collection\CollectionWidget;
 
 /** @var \ZnBundle\Messenger\Domain\Entities\MessageEntity[] $collection */
 $collection = $dataProvider->getCollection();
@@ -30,11 +27,14 @@ $collection = $dataProvider->getCollection();
 $authService = \ZnCore\Base\Libs\App\Helpers\ContainerHelper::getContainer()->get(\ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface::class);
 $myId = $authService->getIdentity()->getId();
 
+$this->registerJs('
+
+');
 
 ?>
 
-<div class="card card-primary card-outline direct-chat direct-chat-primary" style="width: 400px;">
-    <div class="card-header">
+<div class="card card-primary card-outline11 direct-chat direct-chat-primary" style="width: auto;">
+    <!--<div class="card-header">
         <h3 class="card-title">Direct Chat</h3>
         <div class="card-tools">
             <span data-toggle="tooltip" title="3 New Messages" class="badge badge-light">3</span>
@@ -48,172 +48,116 @@ $myId = $authService->getIdentity()->getId();
             <button type="button" class="btn btn-tool" data-widget="remove"><i class="fas fa-times"></i>
             </button>
         </div>
-    </div>
+    </div>-->
     <div class="card-body">
-        <div class="direct-chat-messages">
+        <div class="direct-chat-messages" style="height: 318px;">
             <?= $this->renderFile(__DIR__ . '/_messages.php', [
-                    'collection' => $collection,
-                    'myId' => $myId,
+                'collection' => $collection,
+                'myId' => $myId,
             ]) ?>
         </div>
         <!--/.direct-chat-messages-->
         <div class="direct-chat-contacts">
-            <ul class="contacts-list">
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user1-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    Count Dracula
-                    <small class="contacts-list-date float-right">2/28/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">How have you been? I was...</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user7-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    Sarah Doe
-                    <small class="contacts-list-date float-right">2/23/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">I will be waiting for...</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user3-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    Nadia Jolie
-                    <small class="contacts-list-date float-right">2/20/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">I'll call you back at...</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user5-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    Nora S. Vans
-                    <small class="contacts-list-date float-right">2/10/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">Where is your new...</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user6-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    John K.
-                    <small class="contacts-list-date float-right">1/27/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">Can I take a look at...</span>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img class="contacts-list-img" src="https://adminlte.io/docs/3.0/assets/img/user8-128x128.jpg">
-                        <div class="contacts-list-info">
-                  <span class="contacts-list-name">
-                    Kenneth M.
-                    <small class="contacts-list-date float-right">1/4/2015</small>
-                  </span>
-                            <span class="contacts-list-msg">Never mind I found...</span>
-                        </div>
-                    </a>
-                </li>
-            </ul>
+            <?= $this->renderFile(__DIR__ . '/_contacts.php') ?>
         </div>
     </div>
     <div class="card-footer">
-        <form id="messageForm" action="/messenger/send-message" method="post" onsubmit="sendMessage(this); return false;">
-            <div class="input-group">
-                <input type="hidden" name="chatId" value="<?= $formView->vars['value']->getChatId() ?>">
-                <input type="text" name="message" placeholder="Type Message ..." class="form-control">
-                <span class="input-group-append">
-              <button type="button" class="btn btn-primary" name="submit">Send</button>
-            </span>
-            </div>
-        </form>
+        <?= $this->renderFile(__DIR__ . '/_form.php', [
+            'formView' => $formView,
+        ]) ?>
     </div>
 </div>
 
 <script>
 
-    var socket = new WebSocket("ws://127.0.0.1:8001?userId=1");
-    socket.onopen = function() {
-        console.log("Соединение установлено.");
-    };
-    socket.onclose = function(event) {
-        if (event.wasClean) {
-            console.log('Соединение закрыто чисто');
-        } else {
-            console.log('Обрыв соединения'); // например, "убит" процесс сервера
-        }
-        console.log('Код: ' + event.code + ' причина: ' + event.reason);
-    };
-    socket.onmessage = function(event) {
-        var data = JSON.parse(event.data);
-        var eventName = data.name;
-        var eventData = data.data;
-        //console.log("New message " + data.name);
-        if(eventName == 'sendMessage') {
-            updateMessageList(eventData.chatId);
-            console.log("New message " + eventData.chatId);
-        }
-        console.log("Получены данные " + event.data);
-    };
-    socket.onerror = function(error) {
-        console.log("Ошибка " + error.message);
-    };
+    document.addEventListener('DOMContentLoaded', function () { // Аналог $(document).ready(function(){
 
-
-    function sendMessage() {
-        // var formElement = $(form);
-        var formElement = $('#messageForm');
-        var action = formElement.attr('action');
-        var textElement = formElement.find('input[name=message]');
-        var chatIdElement = formElement.find('input[name=chatId]');
-        var chatId = chatIdElement.val();
-        var text = textElement.val();
-
-        $.ajax({
-            type: 'POST',
-            url: action,
-            data: {
-                'text': text,
-                'chatId': chatId,
-            },
-            success: function(msg) {
-                textElement.val('');
-                // updateMessageList();
-                //alert('wow' + msg);
+        var socket = new WebSocket("ws://127.0.0.1:8001?userId=1");
+        socket.onopen = function () {
+            console.log("Соединение установлено.");
+        };
+        socket.onclose = function (event) {
+            if (event.wasClean) {
+                console.log('Соединение закрыто чисто');
+            } else {
+                console.log('Обрыв соединения'); // например, "убит" процесс сервера
             }
-        });
-    }
-
-    function updateMessageList() {
-        var formElement = $('#messageForm');
-        var chatIdElement = formElement.find('input[name=chatId]');
-        var chatId = chatIdElement.val();
-        $.ajax({
-            type: 'GET',
-            url: '/messenger/message-list/?chatId=' + chatId,
-            success: function(msg) {
-                var messageList = $('.direct-chat-messages');
-                messageList.html(msg);
-                messageList.scrollTop(messageList.prop("scrollHeight"));
+            console.log('Код: ' + event.code + ' причина: ' + event.reason);
+        };
+        socket.onmessage = function (event) {
+            var data = JSON.parse(event.data);
+            var eventName = data.name;
+            var eventData = data.data;
+            //console.log("New message " + data.name);
+            if (eventName == 'sendMessage') {
+                updateMessageList(eventData.chatId);
+                console.log("New message " + eventData.chatId);
             }
+            console.log("Получены данные " + event.data);
+        };
+        socket.onerror = function (error) {
+            console.log("Ошибка " + error.message);
+        };
+
+
+        function sendMessage() {
+            var formElement = $('#messageForm');
+            var action = formElement.attr('action');
+            var textElement = formElement.find('input[name=message]');
+            var chatIdElement = formElement.find('input[name=chatId]');
+            var chatId = chatIdElement.val();
+            var text = textElement.val();
+
+            $.ajax({
+                type: 'POST',
+                url: action,
+                data: {
+                    'text': text,
+                    'chatId': chatId,
+                },
+                success: function (msg) {
+                    textElement.val('');
+                }
+            });
+        }
+
+        function updateMessageList() {
+            var formElement = $('#messageForm');
+            var chatIdElement = formElement.find('input[name=chatId]');
+            var chatId = chatIdElement.val();
+            $.ajax({
+                type: 'GET',
+                url: '/messenger/message-list/?chatId=' + chatId,
+                success: function (msg) {
+                    setMessageList(msg);
+                    scrollBottomMessageList();
+                }
+            });
+        }
+
+        function setMessageList(msg) {
+            var messageList = $('.direct-chat-messages');
+            messageList.html(msg);
+        }
+
+        function scrollBottomMessageList() {
+            var messageList = $('.direct-chat-messages');
+            messageList.scrollTop(messageList.prop("scrollHeight"));
+        }
+
+        //scrollBottomMessageList();
+
+        var formElement = $('#messageForm');
+        formElement.submit(function () {
+            sendMessage();
+            return false;
         });
-    }
+
+        $('.direct-chat-messages').show(function () {
+            scrollBottomMessageList();
+            return false;
+        });
+
+    });
 
 </script>
